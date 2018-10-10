ccc49432cb77b479d2f3efb45ae8ed041215d4c8
EC2 Default User <ec2-user@ip-172-31-5-80.us-east-2.compute.internal>
2018-10-10 01:32:26 +0000

    Part 1 and Part 2 A

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -29,7 +30,26 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(/,\s*/).each do |rating|
+    rating = "ratings_#{rating}"
+    if uncheck
+      uncheck(rating)
+    else
+      check(rating)
+    end
+  end
+  #fail "Unimplemented"
+end
+
+Then /I should see movies rated: (,*)/ do |rating_list|
+  rating_list.split(/,\s*/).each do |rating|
+    assert page.has_xpath?('//*', :text => "#{rating}")
+  end
+end
+ Then /I should not see movies rated: (,*)/ do |rating_list|
+  rating_list.split(/,\s*/).each do |rating|
+    assert page.has_no_xpath?('//*', :text => "#{rating}")
+  end
 end
 
 Then /I should see all the movies/ do
