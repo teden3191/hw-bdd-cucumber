d7d058cb6acb5ef6f155e4907ba19d399f54c0e6
EC2 Default User <ec2-user@ip-172-31-5-80.us-east-2.compute.internal>
2018-10-14 17:43:19 +0000

    finished part 2

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -19,35 +19,56 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  
+  #fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+#When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  rating_list.split(/,\s*/).each do |rating|
+
+When /I check the following ratings: (.*)/ do |rating_list|
+  rating_list.split().each do |rating|
     rating = "ratings_#{rating}"
-    if uncheck
-      uncheck(rating)
-    else
-      check(rating)
-    end
+    check(rating)
   end
 #  fail "Unimplemented"
 end
 
-Then /I should see movies rated: (,*)/ do |rating_list|
+When /I uncheck the following ratings: (.*)/ do |rating_list|
+  rating_list.split().each do |rating|
+    rating = "ratings_#{rating}"
+    uncheck(rating)
+  end
+#  fail "Unimplemented"
+end
+
+Then /I should see the following movies: (.*)/ do |title_list|
+  title_list.split(/,\s*/).each do |title|
+    expect(page).to have_content(title)
+    #assert page.has_xpath?('//*', :text => "#{title}")
+  end
+end
+
+Then /I should not see the following movies: (.*)/ do |title_list|
+  title_list.split(/,\s*/).each do |title|
+    expect(page).to have_no_content(title)
+    #assert page.has_no_xpath?('//*', :text => "#{title}")
+  end
+end
+
+Then /I should see movies rated: (.*)/ do |rating_list|
   rating_list.split(/,\s*/).each do |rating|
     assert page.has_xpath?('//*', :text => "#{rating}")
   end
 end
 
-Then /I should not see movies rated: (,*)/ do |rating_list|
+Then /I should not see movies rated: (.*)/ do |rating_list|
   rating_list.split(/,\s*/).each do |rating|
     assert page.has_no_xpath?('//*', :text => "#{rating}")
   end
@@ -55,5 +76,7 @@ end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  rows = page.all('table#movies tr').count
+  rows.should == 11
+#  fail "Unimplemented"
 end
